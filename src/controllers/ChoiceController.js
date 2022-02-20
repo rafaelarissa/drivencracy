57bd144f037c9fc0c004b33ad0277aa5e3deb9db
rafaelarissa <rafaela.larissalj@gmail.com>
2022-02-20 16:24:01 -0300

    feat: added validation to not permit empty values

--- src/controllers/ChoiceController.js
+++ src/controllers/ChoiceController.js
@@ -1,7 +1,19 @@
+import Joi from "joi";
 import db from "../db.js";
 
+const choiceSchema = Joi.object({
+  title: Joi.string().required().trim(),
+  poolId: Joi.number().required()
+})
 
 export async function setChoice(req, res) {
+  const validation = choiceSchema.validate(req.body);
+
+  if(validation.error) {
+    res.sendStatus(422);
+    return
+  }
+
   const choice = {
     title: req.body.title,
     poolId: req.body.poolId
